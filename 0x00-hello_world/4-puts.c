5b91ce713fe3a698fd3255a1eff252a028c7c782
Ayyoub <azimayyoub@gmail.com>
2023-10-05 18:54:50 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*woring with puts */
 int main(void)
 { 
-     puts("\"Programming is like building a multilingual puzzle"); 
-     return (0); 
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0); 
 }
