29988f93a225eb4cfe281e734c3ad41b557141ad
Ayyoub <azimayyoub@gmail.com>
2023-10-06 06:15:08 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#include 
+#include <stdio.h>
+
+/**
+ * main - Entry pointk
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
