5dbd3eb0cff765f22eaf8f90cfcd788ebc6c78fc
Ayyoub <azimayyoub@gmail.com>
2023-10-05 19:56:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main(void) 
-{   /* main function printing a string*/ 
+int main(void) {
+   /* main function printing a string*/ 
     puts("\"Programming is like building a multilingual puzzle");
     return (0); 
 }
