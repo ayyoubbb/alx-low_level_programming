fc1e3dc5d3c0983edd9be4138755647c7546a2f0
Ayyoub <azimayyoub@gmail.com>
2023-10-06 10:09:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main -entry point
+  *
+  *A C program tha prints a string to stdout using fprintd
+  *
+  *Return: always 1(failure)
+  */
+
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
