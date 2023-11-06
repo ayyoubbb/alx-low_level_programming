e326c87663e87e69296a352377c3c9c4b074242d
Ayyoub <azimayyoub@gmail.com>
2023-11-06 13:09:15 -0800

    a new type struct dog

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -14,6 +14,6 @@
 	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
 	my_dog.owner = "Bob";
-	printf("My name is %s, and I am %.1f :) - woof!\n", my_dog.name, my_dog.age);
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	return (0);
 }
