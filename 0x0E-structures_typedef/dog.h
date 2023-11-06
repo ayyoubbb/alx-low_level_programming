62e9ff817b8e1b6c751c11dad82513b5e1cfa6b3
Ayyoub <azimayyoub@gmail.com>
2023-11-06 12:44:26 -0800

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - define three types of elements
+ * @name: first parameter of char.
+ * @age: second parameter of float
+ * @owner: third paramter of chr
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
